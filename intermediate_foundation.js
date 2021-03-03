1131a47785bce543783c50bd10dc1ab3997b38ac
Christie Caiano <christiecaiano@gmail.com>
2021-03-03 18:02:24 -0500

    Added Double Trouble function

--- intermediate_foundation.js
+++ intermediate_foundation.js
@@ -53,3 +53,13 @@ console.log(nthToLast([4,1,4,6,7,2,9,4],4));
 
 
 // Double Trouble: Create a function that changes a given array to list each existing element twice, retaining original order.  Convert [4, "Ulysses", 42, false] to [4,4, "Ulysses", "Ulysses", 42, 42, false, false].
+
+    function doubleTrouble (array){
+        let newArr = [];
+        for (let i = 0; i < array.length; i++) {
+            newArr.push(array[i], array[i]);
+        }
+        return newArr;
+    }
+
+    console.log(doubleTrouble([4,"Ulysses", 42, false]));
